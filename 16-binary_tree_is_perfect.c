1f4c0c5c4d25fa9348a4a887deda538e931ec533
Jenni-Foued <medfouedjenni@gmail.com>
2021-03-10 06:05:24 +0100

    Fixed betty style by adding variables

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,12 +32,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	binary_tree_t *right, *left;
+
+	right = tree->right;
+	left = tree->left;
 	if (!(tree))
 		return (0);
 	if (tree->right == NULL && tree->left == NULL)
 		return (1);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(left) && binary_tree_is_perfect(right))
 			return (1);
 	return (0);
 }
